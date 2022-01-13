737f6e238a2ea8995f8f04d3057f01a8ae2b50fc
Jagruti Anil Ghadi <jagruti.g.1245@gmail.com>
2022-01-14 00:28:43 +0530

    Added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+package LetsLearnGit;
+
+public class Multiply {
+    public static void main(String[] args) {
+        int a=4;
+        int b=3;
+
+        System.out.println("The product is:"+ (a*b));
+    }
+}
