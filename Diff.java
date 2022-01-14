ae4891dca9181b6434911106be4f9521d9f2578d
Jagruti Anil Ghadi <jagruti.g.1245@gmail.com>
2022-01-14 15:51:59 +0530

    diff file is modified,added values

--- Diff.java
+++ Diff.java
@@ -2,6 +2,9 @@ package LetsLearnGit;
 
 public class Diff {
     public static void main(String[] args) {
-        
+        int a=10;
+        int b=5;
+
+        System.out.println("Difference is:"+(a-b));
     }
 }
